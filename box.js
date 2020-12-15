96a6008cfbde151633726f1a10a9eb67171df574
rudra <74863312+rptyagi26@users.noreply.github.com>
2020-12-15 18:54:46 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box
+{
+    constructor(x,y,w,h)
+    {
+      this.body=Bodies.rectangle(x,y,w,h);
+      this.width=w;
+      this.height=h
+      World.add(world, this.body);
+    }
+
+    display()
+    {  var pos= this.body.position;
+        var angle= this.body.angle;
+        push();
+       translate(pos.x,pos.y)
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+
+        pop();
+    }
+}
